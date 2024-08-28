4f9c245add27ec74aaafd822d364db024adfc44c
Mika Griebsch <mika.griebsch@gmail.com>
2024-08-28 21:13:47 +0200

    Title geändert

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Praxis für Frauen - Dr. Dagmar Roßner';
+export const SITE_DESCRIPTION = 'Willkommen auf der Website!';
