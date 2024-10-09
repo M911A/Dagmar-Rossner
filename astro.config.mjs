61f46009c5ebd3fa78724139dd299911f04a3733
Mika Griebsch <mika.griebsch@gmail.com>
2024-10-09 15:32:02 +0200

    kKleinigkeiten

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://frauenarzt-dr-dagmar-rossner.de/',
 	integrations: [mdx(), sitemap()],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Praxis für Frauen - Dr. Dagmar Roßner';
-export const SITE_DESCRIPTION = 'Willkommen auf der Website!';
+export const SITE_DESCRIPTION = 'Willkommen auf der Website für Frauen!';
